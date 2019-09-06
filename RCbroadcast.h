97de274e90888c8493523a562d5956f6a0466a05
audacity23 <53383233+audacity23@users.noreply.github.com>
2019-09-06 20:17:59 +0530

    Update RCbroadcast.h

--- RCbroadcast.h
+++ RCbroadcast.h
@@ -9,6 +9,8 @@
 typedef nx_struct BroadcastbyCC {	//new
   nx_uint16_t CCid;
  nx_uint16_t duplicateCCid;
+ nx_uint16_t x;
+ nx_uint16_t y;
 } BroadcastbyCC;
 
 typedef nx_struct RCtoCCMsg {	//RC to CC message structure
